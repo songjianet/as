06f3a2c3b8d6aec2db9e5407352f56a1a995cf1b
songjian <1778651752@qq.com>
2019-07-12 17:00:12 +0800

    feat(src/xhr.ts): add request error handling
    add request error handling
    
    BREAKING CHANGE: add request error handling

--- src/xhr.ts
+++ src/xhr.ts
@@ -9,7 +9,7 @@ import { parseHeaders } from './helpers/headers'
  * @author songjianet
  */
 export default function xhr(config: AsRequestConfig): AsPromise {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const { data = null, url, method = 'get', headers, responseType } = config
 
     const request = new XMLHttpRequest()
@@ -18,8 +18,10 @@ export default function xhr(config: AsRequestConfig): AsPromise {
       request.responseType = responseType
     }
 
+    // 设置请求信息
     request.open(method.toUpperCase(), url, true)
 
+    // 包装返回数据
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
         return
@@ -37,6 +39,12 @@ export default function xhr(config: AsRequestConfig): AsPromise {
       resolve(response)
     }
 
+    // 处理错误请求
+    request.onerror = function handleError() {
+      reject(new Error('Network Error!'))
+    }
+
+    // 处理请求头
     Object.keys(headers).forEach(n => {
       if (data === null && n.toLowerCase() === 'content-type') {
         delete headers[n]
@@ -45,6 +53,7 @@ export default function xhr(config: AsRequestConfig): AsPromise {
       }
     })
 
+    // 发送请求
     request.send(data)
   })
 }
