5229e9ebcd6e377f6319287d8e0d23531fee0c1a
songjian <1778651752@qq.com>
2019-07-16 11:39:12 +0800

    test(examples/server.js examples/error/): test request error
    test request timeout and network error

---
+++ examples/error/app.ts
@@ -0,0 +1,40 @@
+import as from '../../src/index'
+
+as({
+  method: 'get',
+  url: '/error/get1'
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e)
+})
+
+as({
+  method: 'get',
+  url: '/error/get'
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e)
+})
+
+setTimeout(() => {
+  as({
+    method: 'get',
+    url: '/error/get'
+  }).then((res) => {
+    console.log(res)
+  }).catch((e) => {
+    console.log(e)
+  })
+}, 5000)
+
+as({
+  method: 'get',
+  url: '/error/timeout',
+  timeout: 2000
+}).then((res) => {
+  console.log(res)
+}).catch((e) => {
+  console.log(e)
+})
