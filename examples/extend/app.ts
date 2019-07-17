0d335c7273aca270f77500e2fafd3f507745a5ad
songjian <1778651752@qq.com>
2019-07-17 11:19:06 +0800

    test(examples/extend/ examples/index.html examples/server.js): test request method
    test request method

---
+++ examples/extend/app.ts
@@ -0,0 +1,31 @@
+import as from '../../src/index'
+
+as({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hi'
+  }
+})
+
+as.request({
+  url: '/extend/post',
+  method: 'post',
+  data: {
+    msg: 'hello'
+  }
+})
+
+as.get('/extend/get')
+
+as.options('/extend/options')
+
+as.delete('/extend/delete')
+
+as.head('/extend/head')
+
+as.post('/extend/post', { msg: 'post' })
+
+as.put('/extend/put', { msg: 'put' })
+
+as.patch('/extend/patch', { msg: 'patch' })
