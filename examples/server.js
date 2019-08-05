cf9d73984cb47116ee34380f1c2607100d34bcca
songjian <1778651752@qq.com>
2019-08-05 09:54:21 +0800

    test(examples/more/app.ts examples/server.js): test xsrf
    test xsrf but there may by bug

--- examples/server.js
+++ examples/server.js
@@ -21,7 +21,11 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(webpackHotMiddleware(compiler))
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname, {
+  setHeader (res) {
+    res.cookie('XSRF-TOKEN-D', '1234abc')
+  }
+}))
 
 app.use(bodyParser.json())
 // app.use(bodyParser.text())
