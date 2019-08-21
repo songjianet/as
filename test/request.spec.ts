02f260350e8e15e775fc94c227ea61e5e40878ae
songjian <1778651752@qq.com>
2019-08-21 20:58:49 +0800

    test(test/request.spec.ts): add unit testing for request mode
    add unit testing for request mode

--- test/request.spec.ts
+++ test/request.spec.ts
@@ -54,7 +54,7 @@ describe('requests', () => {
       expect(resolveSpy).not.toHaveBeenCalled()
       expect(rejectSpy).toHaveBeenCalled()
       expect(reason instanceof Error).toBeTruthy()
-      expect((reason as AsError).message).toBe('Network Error')
+      expect((reason as AsError).message).toBe('Network Error!')
       expect(reason.request).toEqual(expect.any(XMLHttpRequest))
 
       jasmine.Ajax.install()
@@ -69,6 +69,184 @@ describe('requests', () => {
       method: 'post'
     }).catch(error => {
       err = error
+    })
+
+    getAjaxRequest().then(request => {
+      // @ts-ignore
+      request.eventBus.trigger('timeout')
+
+      setTimeout(() => {
+        expect(err instanceof Error).toBeTruthy()
+        expect(err.message).toBe('Timeout of 2000ms exceeded!')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should reject when validateStatus returns false', () => {
+    const resolveSpy = jest.fn((res: AsResponseConfig) => {
+      return res
+    })
+
+    const rejectSpy = jest.fn((e: AsError) => {
+      return e
+    })
+
+    as('/foo', {
+      validateStatus(status) {
+        return status !== 500
+      }
+    })
+      .then(resolveSpy)
+      .catch(rejectSpy)
+      .then(next)
+
+    return getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 500
+      })
+    })
+
+    function next(reason: AsError | AsResponseConfig) {
+      expect(resolveSpy).not.toHaveBeenCalled()
+      expect(rejectSpy).toHaveBeenCalled()
+      expect(reason instanceof Error).toBeTruthy()
+      expect((reason as AsError).message).toBe('Request failed with status code 500!')
+      expect((reason as AsError).response!.status).toBe(500)
+    }
+  })
+
+  test('should resolve when validateStatus returns true', () => {
+    const resolveSpy = jest.fn((res: AsResponseConfig) => {
+      return res
+    })
+
+    const rejectSpy = jest.fn((e: AsError) => {
+      return e
+    })
+
+    as('/foo', {
+      validateStatus(status) {
+        return status === 500
+      }
+    })
+      .then(resolveSpy)
+      .catch(rejectSpy)
+      .then(next)
+
+    return getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 500
+      })
+    })
+
+    function next(res: AsResponseConfig | AsError) {
+      expect(resolveSpy).toHaveBeenCalled()
+      expect(rejectSpy).not.toHaveBeenCalled()
+      expect(res.config.url).toBe('/foo')
+    }
+  })
+
+  test('should return JSON when resolved', done => {
+    let response: AsResponseConfig
+
+    as('/api/account/signup', {
+      auth: {
+        username: '',
+        password: ''
+      },
+      method: 'post',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        statusText: 'OK',
+        responseText: '{"error": 0}'
+      })
+
+      setTimeout(() => {
+        expect(response.data).toEqual({ error: 0 })
+        done()
+      }, 100)
+    })
+  })
+
+  test('should return JSON when rejecting', done => {
+    let response: AsResponseConfig
+
+    as('/api/account/signup', {
+      auth: {
+        username: '',
+        password: ''
+      },
+      method: 'post',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).catch(error => {
+      response = error.response
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 400,
+        statusText: 'Bad Request',
+        responseText: '{"error": "BAD USERNAME", "code": 1}'
+      })
+
+      setTimeout(() => {
+        expect(typeof response.data).toBe('object')
+        expect(response.data.error).toBe('BAD USERNAME')
+        expect(response.data.code).toBe(1)
+        done()
+      }, 100)
+    })
+  })
+
+  test('should supply correct response', done => {
+    let response: AsResponseConfig
+
+    as.post('/foo').then(res => {
+      response = res
+    })
+
+    getAjaxRequest().then(request => {
+      request.respondWith({
+        status: 200,
+        statusText: 'OK',
+        responseText: '{"foo": "bar"}',
+        responseHeaders: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      setTimeout(() => {
+        expect(response.data.foo).toBe('bar')
+        expect(response.status).toBe(200)
+        expect(response.statusText).toBe('OK')
+        expect(response.headers['content-type']).toBe('application/json')
+        done()
+      }, 100)
+    })
+  })
+
+  test('should allow overriding Content-Type header case-insensitive', () => {
+    let response: AsResponseConfig
+
+    as.post('/foo', { prop: 'value' }, { headers: { 'content-type': 'application/json' } }).then(
+      res => {
+        response = res
+      }
+    )
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['Content-Type']).toBe('application/json')
     })
   })
 })
