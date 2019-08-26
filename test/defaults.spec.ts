bc18b41b30f421c61c08d0f638245ffdefdb5e31
songjian <1778651752@qq.com>
2019-08-26 20:08:14 +0800

    test(test/defaults.spec.ts): add unit testing for defaults model
    add unit testing for defaults model

--- test/defaults.spec.ts
+++ test/defaults.spec.ts
@@ -22,9 +22,137 @@ describe('defaults', () => {
   })
 
   test('should transform response json', () => {
-    const data = (as.defaults.transformResponse as AsTransformer[])[0]('foo=bar')
+    const data = (as.defaults.transformResponse as AsTransformer[])[0]('{"foo":"bar"}')
 
     expect(typeof data).toBe('object')
     expect(data.foo).toBe('bar')
+  })
+
+  test('should do nothing to response string', () => {
+    expect((as.defaults.transformResponse as AsTransformer[])[0]('foo=bar')).toBe('foo=bar')
+  })
+
+  test('should use global defaults config', () => {
+    as('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+    })
+  })
+
+  test('should use modified defaults config', () => {
+    as.defaults.baseURL = 'http://example.com/'
+
+    as('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('http://example.com/foo')
+      delete as.defaults.baseURL
+    })
+  })
+
+  test('should use request config', () => {
+    as('/foo', {
+      baseURL: 'http://www.example.com'
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('http://www.example.com/foo')
+    })
+  })
+
+  test('should use default config for custom instance', () => {
+    const instance = as.create({
+      xsrfCookieName: 'CUSTOM-XSRF-TOKEN',
+      xsrfHeaderName: 'X-CUSTOM-XSRF-TOKEN'
+    })
+    document.cookie = instance.defaults.xsrfCookieName + '=foobarbaz'
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders[instance.defaults.xsrfHeaderName!]).toBe('foobarbaz')
+      document.cookie =
+        instance.defaults.xsrfCookieName +
+        '=;expires=' +
+        new Date(Date.now() - 86400000).toUTCString()
+    })
+  })
+
+  test('should use GET headers', () => {
+    as.defaults.headers.get['X-CUSTOM-HEADER'] = 'foo'
+    as.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['X-CUSTOM-HEADER']).toBe('foo')
+      delete as.defaults.headers.get['X-CUSTOM-HEADER']
+    })
+  })
+
+  test('should use POST headers', () => {
+    as.defaults.headers.post['X-CUSTOM-HEADER'] = 'foo'
+    as.post('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders['X-CUSTOM-HEADER']).toBe('foo')
+      delete as.defaults.headers.post['X-CUSTOM-HEADER']
+    })
+  })
+
+  test('should use header config', () => {
+    const instance = as.create({
+      headers: {
+        common: {
+          'X-COMMON-HEADER': 'commonHeaderValue'
+        },
+        get: {
+          'X-GET-HEADER': 'getHeaderValue'
+        },
+        post: {
+          'X-POST-HEADER': 'postHeaderValue'
+        }
+      }
+    })
+
+    instance.get('/foo', {
+      headers: {
+        'X-FOO-HEADER': 'fooHeaderValue',
+        'X-BAR-HEADER': 'barHeaderValue'
+      }
+    })
+
+    return getAjaxRequest().then(request => {
+      expect(request.requestHeaders).toEqual(
+        deepMerge(as.defaults.headers.common, as.defaults.headers.get, {
+          'X-COMMON-HEADER': 'commonHeaderValue',
+          'X-GET-HEADER': 'getHeaderValue',
+          'X-FOO-HEADER': 'fooHeaderValue',
+          'X-BAR-HEADER': 'barHeaderValue'
+        })
+      )
+    })
+  })
+
+  test('should be used by custom instance if set before instance created', () => {
+    as.defaults.baseURL = 'http://example.org/'
+    const instance = as.create()
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('http://example.org/foo')
+      delete as.defaults.baseURL
+    })
+  })
+
+  test('should not be used by custom instance if set after instance created', () => {
+    const instance = as.create()
+    as.defaults.baseURL = 'http://example.org/'
+
+    instance.get('/foo')
+
+    return getAjaxRequest().then(request => {
+      expect(request.url).toBe('/foo')
+    })
   })
 })
